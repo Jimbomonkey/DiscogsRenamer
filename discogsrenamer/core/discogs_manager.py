d81ee0657ad9a62ae8fc70aed547f810cee05212
jimbomonkey <annex12@hotmail.com>
2026-05-14 19:48:45 +0100

    No call requires authorisation so error check is redundant

--- discogsrenamer/core/discogs_manager.py
+++ discogsrenamer/core/discogs_manager.py
@@ -1,5 +1,5 @@
 from discogs_client import Client, Release, Track, Artist
-from discogs_client.exceptions import HTTPError, AuthorizationError
+from discogs_client.exceptions import HTTPError
 
 from discogsrenamer.gui.widgets.release_list_item import ReleaseListItem
 from discogsrenamer.core.models.release_data import ReleaseData
@@ -23,10 +23,9 @@ class DiscogsManager:
             # To capture a failed fetch, the exception needs to
             # be forced by calling for non-existent data
             _ = release.title
-        except AuthorizationError:
-            # Subclass of HTTPError, so must be handled first
-            return None
         except HTTPError:
+            # A message is displayed during the
+            # populate step if None is returned here
             return None
         return release
 
